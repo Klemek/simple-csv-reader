8ddc4c3417918a14877089c666d5e00666534d9b
Klemek <Klemek@outlook.fr>
2019-09-23 00:54:43 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #*/5 * * * * /var/www/html/speedtest.sh >> /var/www/html/speedtest.log 2>&1
 echo "started"
-/usr/local/bin/speedtest-cli --single --csv --bytes >> /var/www/html/speedtest.csv
+/usr/local/bin/speedtest-cli --single --csv >> /var/www/html/speedtest.csv
 echo "done"
 
 
