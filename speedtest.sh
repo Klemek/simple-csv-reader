9bb6b390d6c0e7f2fd7bedca6aaca9cf14934b77
= <=>
2019-09-23 00:42:18 +0200

    simplified process

--- speedtest.sh
+++ speedtest.sh
@@ -2,5 +2,5 @@
 
 #*/5 * * * * . /var/www/html/speedtest.sh >> /var/www/html/speedtest.log 2>&1
 
-/usr/bin/python /var/www/html/speedtest.py >> /var/www/html/speedtest.csv
+/usr/local/bin/speedtest-cli --single --csv --bytes >> /var/www/html/speedtest.csv
 echo done
