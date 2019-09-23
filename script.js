2dc30fcfb02a6f0ab886abc4279095803c3a11d8
Klemek <klemek@outlook.fr>
2019-09-23 14:13:57 +0200

    road to generic loading

--- script.js
+++ script.js
@@ -1,49 +1,94 @@
-const red = 'rgb(255, 99, 132)';
-const blue = 'rgb(54, 162, 235)';
-const SAMPLE_SIZE = 1e5;
-const CSV_SIZE = 8;
-const CSV_DATE = 3;
-const CSV_DOWNLOAD = 6;
-const CSV_UPLOAD = 7;
+// === CONFIG ===
+
+//colors
+
+//general
 const TIME_STEP = 15; //minutes
 
-const datasetNames = {
-  all: 'All time',
-  month: 'Month',
-  week: 'Week',
-  day: '24H',
-};
+//CSV data
+const CSV_PATH = './speedtest.csv'; // path accessible by ajax request
+const CSV_SIZE = 8; // line max size
+const CSV_DATE = 3; // column containing the date
+const CSV_COLUMNS = [6, 7];
 
-const datasetButtons = {
-  all: 'allTime',
-  month: 'lastMonth',
-  week: 'lastWeek',
-  day: 'lastDay',
+//Display data
+const GRAPH_TITLE = 'Speed-Test';
+const COLUMNS_NAME = ['Download', 'Upload'];
+const COLUMNS_FACTOR = [1 / 8e6, 1 / 8e6];
+const COLUMNS_FIXED = [3, 3];
+const COLORS = [
+  'rgb(255,99,132)',
+  'rgb(54, 162, 235)'
+];
+const Y_UNIT = 'MBps';
+
+// === ADVANCED CONFIG ===
+
+const MINIMUM_DATA = 5; //hide datasets with too few data
+const SAMPLE_SIZE = 1e5; //how many sample data generated
+
+// ==== SCRIPT (do not edit) ===
+
+const datasets = {
+  all: {
+    name: 'All time',
+    max: 100,
+  },
+  year: {
+    name: 'Year',
+    max: 12 * 8,
+    time: 365.25 * 24 * 60,
+    format: 'ddd dd mmm yyyy'
+  },
+  month: {
+    name: 'Month',
+    max: 31,
+    time: 30.5 * 24 * 60,
+    format: 'ddd dd mmm'
+  },
+  week: {
+    name: 'Week',
+    max: 7 * 8,
+    time: 7 * 24 * 60,
+    format: 'HH"H" ddd dd mmm'
+  },
+  day: {
+    name: '24H',
+    max: 24,
+    time: 24 * 60,
+    format: 'HH:MM ddd dd'
+  },
+  hour: {
+    name: '1H',
+    max: 60,
+    time: 60,
+    format: 'HH:MM'
+  },
+  minute: {
+    name: '10m',
+    max: 100,
+    time: 10,
+    format: 'HH:MM:ss'
+  },
 };
 
 const config = {
   type: 'line',
   data: {
     labels: [], //00:00 00/00/0000
-    datasets: [{
-      label: 'Download',
-      borderColor: red,
-      backgroundColor: red,
+    datasets: COLUMNS_NAME.map((n, i) => ({
+      label: COLUMNS_NAME[i],
+      borderColor: COLORS[i],
+      backgroundColor: COLORS[i],
       fill: false,
       data: []
-    }, {
-      label: 'Upload',
-      borderColor: blue,
-      backgroundColor: blue,
-      fill: false,
-      data: []
-    }],
+    })),
   },
   options: {
     responsive: true,
     title: {
       display: true,
-      text: 'Speed-Test (loading)'
+      text: `${GRAPH_TITLE} (loading)`
     },
     tooltips: {
       mode: 'index',
@@ -66,7 +111,7 @@ const config = {
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'MBps'
+          labelString: Y_UNIT
         },
         ticks: {
           beginAtZero: true
@@ -87,39 +132,30 @@ function get(url) {
   });
 }
 
-function updateData(dataset) {
-  config.data.labels = window.data[dataset].map(l => l[CSV_DATE].replace(/(\d*)-(\d*)-(\d*)T(\d*):(\d*)(.*)/, '$4:$5 $3/$2/$1')); //00:00 00/00/0000
-  config.data.datasets[0].data = window.data[dataset].map(l => (parseFloat(l[CSV_DOWNLOAD]) / 8e6).toFixed(3));
-  config.data.datasets[1].data = window.data[dataset].map(l => (parseFloat(l[CSV_UPLOAD]) / 8e6).toFixed(3));
-  config.options.title.text = `Speed-Test (${datasetNames[dataset]})${window.data.sample ? ' (Sample data)' : ''}`;
+function updateData(d) {
+  config.data.labels = window.data[d].map(l => new Date(l[CSV_DATE]).format(datasets[d].format));
+  CSV_COLUMNS.forEach((n, i) => {
+    config.data.datasets[i].data = window.data[d].map(l => (parseFloat(l[CSV_COLUMNS[i]]) * COLUMNS_FACTOR[i]).toFixed(COLUMNS_FIXED[i]));
+  });
+  config.options.title.text = `${GRAPH_TITLE} (${datasets[d].name})${window.data.sample ? ' (Sample data)' : ''}`;
+  document.title = config.options.title.text;
   if (window.chart)
     setTimeout(() => {
       window.chart.update();
     });
 }
 
-function createLog(date, download, upload) {
-  const line = new Array(CSV_SIZE).fill(null);
-  line[CSV_DATE] = date;
-  line[CSV_DOWNLOAD] = download;
-  line[CSV_UPLOAD] = upload;
-  return line;
-}
-
 function meanLog(extract) {
-  return extract.reduce((a, b) => createLog(a[CSV_DATE], a[CSV_DOWNLOAD] + b[CSV_DOWNLOAD], a[CSV_UPLOAD] + b[CSV_UPLOAD]))
-    .map((v, i) => (i === CSV_DOWNLOAD || i === CSV_UPLOAD) ? v / extract.length : v);
+  return extract.reduce((a, b) => a.map((v, i) => CSV_COLUMNS.includes(i) ? a[i] + b[i] : v))
+    .map((v, i) => CSV_COLUMNS.includes(i) ? v / extract.length : v);
 }
 
 function crunch(data, maxLen, queryLen) {
   queryLen = Math.min(queryLen || data.length, data.length);
   const start = data.length - queryLen;
   const fragmentSize = Math.round(queryLen / maxLen);
-  if (maxLen >= queryLen || fragmentSize === 1) {
-    console.log('returning data from ' + start + ' to end');
+  if (maxLen >= queryLen || fragmentSize === 1)
     return data.slice(start, data.length);
-  }
-  console.log('computing mean data from ' + start + ' to end with step of ' + fragmentSize);
   const newData = [];
   for (let i = start; i < data.length; i += fragmentSize) {
     newData.push(meanLog(data.slice(i, i + fragmentSize)));
@@ -128,54 +164,76 @@ function crunch(data, maxLen, queryLen) {
 }
 
 function initData(data, sample) {
-  console.log(data.slice(99904).map(x => (x[CSV_DOWNLOAD] / 8e6) + ' ' + (x[CSV_UPLOAD] / 8e6)).join('\n'));
   window.data = {
     sample: sample,
     raw: data,
-    day: crunch(data, 100, 24 * 60 / TIME_STEP),
-    week: crunch(data, 100, 7 * 24 * 60 / TIME_STEP),
-    month: crunch(data, 100, 30.5 * 24 * 60 / TIME_STEP),
-    all: crunch(data, 100)
   };
-  console.log('done');
-  updateData('day');
-  console.log(data.slice(99904).map(x => (x[CSV_DOWNLOAD] / 8e6) + ' ' + (x[CSV_UPLOAD] / 8e6)).join('\n'));
+  for (let d in datasets) {
+    window.data[d] = crunch(data, datasets[d].max, datasets[d].time ? datasets[d].time / TIME_STEP : undefined);
+    if (window.data[d].length < MINIMUM_DATA)
+      datasets[d].btn.setAttribute('disabled', '');
+    else
+      datasets[d].btn.removeAttribute('disabled');
+  }
+
+  console.log('data processed');
+  updateData('all');
+}
+
+function createLog(date, values) {
+  const line = new Array(CSV_SIZE).fill(null);
+  line[CSV_DATE] = date;
+  CSV_COLUMNS.forEach((c, i) => {
+    line[c] = values[i];
+  });
+  return line;
 }
 
 function generateSampleData() {
   const sampleData = [];
-  let sampleDownload = 40e6;
-  let sampleUpload = 10e6;
+  let sample = new Array(CSV_COLUMNS.length).fill(0).map((u, i) => 5 / COLUMNS_FACTOR[i]);
   let sampleDate = new Date();
   sampleDate.setMinutes(sampleDate.getMinutes() - sampleDate.getMinutes() % TIME_STEP - TIME_STEP * SAMPLE_SIZE);
   for (let i = 0; i < SAMPLE_SIZE; i++) {
-    sampleDownload = Math.max(0, sampleDownload + Math.random() * 1e6 - 0.5e6);
-    sampleUpload = Math.max(0, sampleUpload + Math.random() * 1e6 - 0.5e6);
+    sample.forEach((s, i) => {
+      sample[i] = Math.max(0, s + (Math.random() - 0.5) / COLUMNS_FACTOR[i]);
+    });
     sampleDate.setMinutes(sampleDate.getMinutes() + TIME_STEP);
-    sampleData.push(createLog(sampleDate.toISOString(), sampleDownload, sampleUpload));
+    sampleData.push(createLog(sampleDate.toISOString(), sample));
   }
   return sampleData;
 }
 
 window.onload = function () {
-  const ctx = document.getElementById('speedtest').getContext('2d');
+  document.title = GRAPH_TITLE;
+  const ctx = document.getElementById('graph').getContext('2d');
   window.chart = new Chart(ctx, config);
 
-  //register events
-  Object.keys(datasetButtons).forEach(dataset => {
-    document.getElementById(datasetButtons[dataset]).addEventListener('click', () => {
-      updateData(dataset);
+  for (let d in datasets) {
+    const btn = document.createElement('button');
+    btn.innerText = datasets[d].name;
+    btn.addEventListener('click', () => {
+      updateData(d);
     });
-  });
+    datasets[d].btn = btn;
+    document.body.appendChild(btn);
+  }
+
+  document.getElementById('reload').addEventListener('click', load);
 };
 
-get('./speedtest.csv').then((data) => {
-  data = data.trim().split('\n').map(l => l.split(','));
-  if (data.length === 0 || data[0].length < CSV_SIZE) {
+function load() {
+  get(CSV_PATH).then((data) => {
+    data = data.trim().split('\n').map(l => l.split(','));
+    if (data.length === 0 || data[0].length < CSV_SIZE) {
+      initData(generateSampleData(), true);
+    } else {
+      initData(data, false);
+    }
+  }).catch(() => {
     initData(generateSampleData(), true);
-  } else {
-    initData(data, false);
-  }
-}).catch(() => {
-  initData(generateSampleData(), true);
-});
+  });
+}
+
+
+load();
