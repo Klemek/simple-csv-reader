c06ea56baf4ecd87d1b3163c107db799343a787d
Klemek <Klemek@outlook.fr>
2019-10-16 00:10:57 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -2,6 +2,7 @@
 SCRIPT=`realpath -s $0`
 DIR=`dirname ${SCRIPT}`
 echo `date` started
+#/bin/kill `ps aux|grep speedtest|grep -v grep|awk '{print $2}'`
 /usr/local/bin/speedtest-cli --single --csv >> ${DIR}/speedtest.csv
 echo `date` done
 
